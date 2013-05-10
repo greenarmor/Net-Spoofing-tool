5ea0f21c1153195bff3cc278f496f948538a3e8b
Emmanuel Secretaria <greenarmor@live.com>
2013-05-10 19:13:45 +0800

    Create TargetApi.java

---
+++ android/annotation/TargetApi.java
@@ -0,0 +1,13 @@
+package android.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({java.lang.annotation.ElementType.TYPE, java.lang.annotation.ElementType.METHOD, java.lang.annotation.ElementType.CONSTRUCTOR})
+public @interface TargetApi
+{
+  public abstract int value();
+}
